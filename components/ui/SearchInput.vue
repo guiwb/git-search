564091499f5d73dafc2da5964f6daec6a611cdbf
Guilherme Barbosa <gui.wb@hotmail.com>
2021-01-23 21:15:28 -0300

    feat(ui): create search input

---
+++ components/ui/SearchInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    type="text"
+    placeholder="Pesquisar..."
+    :value="value"
+    @input="$emit('input')"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  font-size: 15px;
+  outline: none;
+  border: 1px solid $gray200;
+  height: 35px;
+  border-radius: 4px;
+  padding: 0 10px 0 40px;
+  width: calc(100% - 50px);
+  margin-bottom: 10px;
+  background: url('../../static/images/search.svg') no-repeat 10px;
+}
+</style>
