5cecddd2b769fd513ce7f12e2d10778e6adb256a
Guilherme Barbosa <gui.wb@hotmail.com>
2021-01-25 15:36:30 -0300

    feat(ui): create users list component

---
+++ components/UsersList/card.vue
@@ -0,0 +1,43 @@
+<template>
+  <n-link :to="`/user/${user.login}`">
+    <img :src="user.avatar_url" :alt="user.login" />
+    <h1 v-text="user.login" />
+  </n-link>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+      validator: (value) => 'avatar_url' in value && 'login' in value,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid $gray200;
+  padding: 10px 0;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 50px;
+    object-fit: cover;
+    object-position: center;
+    border: 5px solid $gray100;
+  }
+
+  h1 {
+    font-size: 15px;
+    font-weight: 600;
+    margin-left: 20px;
+    color: $purple400;
+  }
+}
+</style>
---
+++ components/UsersList/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="!users.length" class="no-users">Nenhum usuário encontrado! ;(</div>
+  <div v-else class="list">
+    <Card v-for="user in users" :key="user.login" :user="user" />
+  </div>
+</template>
+
+<script>
+import Card from './card'
+
+export default {
+  components: {
+    Card,
+  },
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.no-users {
+  font-size: 14px;
+  color: $gray400;
+  width: 100%;
+  text-align: center;
+  margin-top: 20px;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
