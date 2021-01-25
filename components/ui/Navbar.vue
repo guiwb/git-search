04a4415102eeb9947e48ca66a5d5a5446fb14cff
Guilherme Barbosa <gui.wb@hotmail.com>
2021-01-25 14:49:24 -0300

    feat(ui): create navbar component

---
+++ components/ui/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <div class="comeback" @click="comeback">
+      <img src="/images/comeback.svg" height="15px" />
+    </div>
+    <h1 v-text="title" />
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    comeback() {},
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  align-items: center;
+  height: 50px;
+  border-bottom: 1px solid $gray100;
+  padding: 0 20px;
+
+  h1 {
+    font-size: 14px;
+    font-weight: 600;
+    margin: 0 0 0 15px;
+  }
+}
+</style>
