17cf1a1507cbb59ab687cf63aec7bdf946463d94
Guilherme Barbosa <gui.wb@hotmail.com>
2021-01-28 00:56:55 -0300

    feat(User): create Projects component

---
+++ components/User/Projects/Item.vue
@@ -0,0 +1,92 @@
+<template>
+  <article>
+    <h1 v-text="repo.name" />
+    <p v-text="description" />
+    <div class="data">
+      <div class="language">
+        <div :style="{ 'background-color': color }" />
+        {{ language }}
+      </div>
+
+      <span>Atualizado em {{ repo.updated_at | formatDate }}</span>
+    </div>
+  </article>
+</template>
+
+<script>
+import getLanguageColor from '../../../helpers/languageColors'
+
+export default {
+  filters: {
+    formatDate(date) {
+      const dateObj = new Date(date)
+
+      const addLeftZero = (number) => (number < 10 ? `0${number}` : number)
+
+      const day = addLeftZero(dateObj.getDate())
+      const month = addLeftZero(dateObj.getMonth() + 1)
+      const year = dateObj.getFullYear()
+
+      return `${day}/${month}/${year}`
+    },
+  },
+  props: {
+    repo: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    color() {
+      return getLanguageColor(this.repo.language) || '#ddd'
+    },
+    description() {
+      return this.repo.description || 'Esse projeto não possui descrição 😕'
+    },
+    language() {
+      return this.repo.language || 'Não definido'
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+article {
+  width: 100%;
+  border-bottom: 1px solid $gray200;
+  padding-bottom: 20px;
+
+  h1 {
+    font-size: 16px;
+    color: $blue400;
+    margin-bottom: 5px;
+  }
+
+  p {
+    font-size: 13px;
+    margin-top: 0;
+  }
+
+  .data {
+    display: flex;
+    justify-content: space-between;
+    font-size: 12px;
+
+    .language {
+      display: flex;
+      align-items: center;
+
+      div {
+        width: 10px;
+        height: 10px;
+        border-radius: 10px;
+        margin-right: 5px;
+      }
+    }
+
+    span {
+      white-space: nowrap;
+    }
+  }
+}
+</style>
---
+++ components/User/Projects/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="repos.length">
+    <Item v-for="repo in repos" :key="repo.id" :repo="repo" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      repos: [],
+    }
+  },
+  async fetch() {
+    this.repos = await this.$http.$get(`users/${this.user.login}/repos`)
+  },
+}
+</script>
